5200f03f6637df9d977675e275a9680ffe91ca20
Eashan <eashan.hatti@yale.edu>
2025-02-22 15:06:06 -0500

    Revert "Fix snapshot spec"
    This reverts commit 81482fa5b6e079a52dd28582e47713fcf2fbd130.

--- Examples/SnapshotSpec.v
+++ Examples/SnapshotSpec.v
@@ -12,39 +12,53 @@ Variant SnapSig {A} : ESig :=
 Arguments SnapSig : clear implicits.
 
 Variant SnapState {T A} :=
-| SnapDef (vs : set A) (m : Name T -> option A).
+| SnapDef (vs : set A) (m : Name T -> option A) (c : Name T -> bool).
 Arguments SnapState : clear implicits.
 
 Variant SnapStep {T A} : SnapState T A -> ThreadEvent T (SnapSig A) -> SnapState T A -> Prop :=
-| SnapCall i v vs m m' :
+| SnapCallPass i v vs m m' c :
+    c i = false ->
     m i = None ->
     m' i = Some v ->
     differ_pointwise m m' i ->
     SnapStep
-      (SnapDef vs m)
+      (SnapDef vs m c)
       (i, CallEv (WriteSnap v))
-      (SnapDef (insert v vs) m')
-| SnapRetPass i v vs m m' :
-  m i = Some v ->
-  m' i = None ->
-  differ_pointwise m m' i ->
-  SnapStep
-    (SnapDef vs m)
-    (i, RetEv (WriteSnap v) (Some vs))
-    (SnapDef vs m')
-| SnapRetFail i v vs m m' :
+      (SnapDef (insert v vs) m' c)
+| SnapRetPass i v vs m m' c c' :
+    c i = false ->
+    c' i = true ->
+    differ_pointwise c c' i ->
     m i = Some v ->
     m' i = None ->
     differ_pointwise m m' i ->
     SnapStep
-      (SnapDef vs m)
+      (SnapDef vs m c)
+      (i, RetEv (WriteSnap v) (Some vs))
+      (SnapDef vs m' c')
+| SnapCallFail i v vs m m' c :
+    c i = true ->
+    m i = None ->
+    m' i = Some v ->
+    differ_pointwise m m' i ->
+    SnapStep
+      (SnapDef vs m c)
+      (i, CallEv (WriteSnap v))
+      (SnapDef vs m' c)
+| SnapRetFail i v vs m m' c :
+    c i = true ->
+    m i = Some v ->
+    m' i = None ->
+    differ_pointwise m m' i ->
+    SnapStep
+      (SnapDef vs m c)
       (i, RetEv (WriteSnap v) None)
-      (SnapDef vs m').
+      (SnapDef vs m' c).
 
 Program Definition snapSpec {T A} : Spec T (SnapSig A) := {|
   State := SnapState T A;
   Step := SnapStep;
-  Init := SnapDef emp (fun _ => None)
+  Init := SnapDef emp (fun _ => None) (fun _ => false)
 |}.
 
 Next Obligation.
@@ -67,8 +81,8 @@ Proof.
     now rewrite H.
   }
   cut (
-    forall p vs m,
-      Steps SnapStep (SnapDef vs m) p s ->
+    forall p vs m c,
+      Steps SnapStep (SnapDef vs m c) p s ->
       SeqConsistent (f m) p
   ).
   { intros. now apply H in H0. }
@@ -81,16 +95,16 @@ Proof.
       (a':= f m').
     {
       subst f. simpl.
-      now rewrite H.
+      now rewrite H0.
     }
     {
       subst f. simpl.
-      now rewrite H0.
+      now rewrite H1.
     }
     { now apply diff. }
     {
       eapply IHp.
-      exact H2.
+      exact H3.
     }
   }
   {
@@ -98,16 +112,33 @@ Proof.
       (a':= f m').
     {
       subst f. simpl.
-      now rewrite H.
+      now rewrite H2.
     }
     {
       subst f. simpl.
-      now rewrite H0.
+      now rewrite H3.
     }
     { now apply diff. }
     {
       eapply IHp.
-      exact H2.
+      exact H5.
+    }
+  }
+  {
+    eapply SCCall with
+      (a':= f m').
+    {
+      subst f. simpl.
+      now rewrite H0.
+    }
+    {
+      subst f. simpl.
+      now rewrite H1.
+    }
+    { now apply diff. }
+    {
+      eapply IHp.
+      exact H3.
     }
   }
   {
@@ -115,16 +146,16 @@ Proof.
       (a':= f m').
     {
       subst f. simpl.
-      now rewrite H.
+      now rewrite H0.
     }
     {
       subst f. simpl.
-      now rewrite H0.
+      now rewrite H1.
     }
     { now apply diff. }
     {
       eapply IHp.
-      exact H2.
+      exact H3.
     }
   }
 Qed.
